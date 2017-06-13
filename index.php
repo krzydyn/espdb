cb718a08036d035ea012376641beae6039ecdd10
krzydyn <krzydyn@gmail.com>
2017-06-13 17:50:45 +0200

    put lighttpd config in htconfig comment

--- index.php
+++ index.php
@@ -16,11 +16,15 @@ $r->addRoute("GET","/index2.php",function() {
 $r->addRoute("GET","/.*",function() {
 	echo "/";
 });
-$r->addRoute("","/.*",function() {
+$r->addRoute("","",function() {
 	echo "default (can't handle)";
 });
 
 echo "<pre>";
+echo "root URI=".Request::getInstance()->getval("cfg.rooturl")."\n";
+echo "absolute URI=".Request::getInstance()->getval("abs-uri")."\n";
+echo "local URI=".Request::getInstance()->getval("uri")."\n";
+print_r(Request::getInstance()->getval("req"));
 //print_r(Request::getInstance()->getval("srv"));
-$r->route(Request::getInstance()->getval("req.method"), Request::getInstance()->getval("uri"));
+$r->route(Request::getInstance()->getval("method"), Request::getInstance()->getval("uri"));
 ?>
